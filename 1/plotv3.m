7d7caab7964904b5b45bd46c13b20397d1909390
unknown <andgrig@student.chalmers.se>
2018-11-11 11:39:17 +0100

    fixed bug

--- 1/plotv3.m
+++ 1/plotv3.m
@@ -1,7 +1,8 @@
 function plotv3(points)
-
+    figure
+    hold on
     for i = 1:size(points, 2)
-        plot3([0,0,0], points(:,i))
+        plot3([0 points(1,i)],[0 points(2,i)],[0 points(3,i)])
     end
 
 end
